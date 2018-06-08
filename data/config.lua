24ad020a20fe1b3b41260c1d9d629aae92800ea8
rakhentash <rakhentash@users.noreply.github.com>
2018-06-09 02:41:27 +0430

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,17 @@
+do local _ = {
+	plug = {
+    "TD"
+  },
+  Helper = "HelperEagleboT",
+  Token = "589283244:AAELmcatNlup3685TrbfODyMDeLvphHyHMI",
+  bot_owner = {
+    545167082,
+    545167082
+  },
+  cmd = "^[/!#]",
+  sudo_users = {
+    545167082
+  }
+}
+return _
+end
